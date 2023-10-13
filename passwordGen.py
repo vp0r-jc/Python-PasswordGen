662c549b3043c9d7481f489fd3368d88b34de40c
Juan C <jcrios@axyz.com.mx>
2023-10-12 22:35:23 -0600

    first commit

---
+++ passwordGen.py
@@ -0,0 +1,20 @@
+#Author __vap0r__
+#PasswordGenerator
+
+import random
+import string
+
+def randomPassword():
+    randomCriteria = string.ascii_letters + string.digits + string.punctuation
+
+    password = random.sample(randomCriteria,8)
+    password += random.sample(string.ascii_uppercase,2)
+    password += random.choice(string.digits)
+    password += random.choice(string.punctuation)
+
+    passwordList = list(password)
+    random.SystemRandom().shuffle(passwordList)
+    password = ' '.join(passwordList)
+    return password
+
+print('Password gen:',randomPassword())
