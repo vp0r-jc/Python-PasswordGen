35dbd77af575f000df82bd905563b9daa161ad36
vp0r-jc <65374911+vp0r-jc@users.noreply.github.com>
2023-10-12 22:40:51 -0600

    Update passwordGen.py

--- passwordGen.py
+++ passwordGen.py
@@ -7,7 +7,7 @@ import string
 def randomPassword():
     randomCriteria = string.ascii_letters + string.digits + string.punctuation
 
-    password = random.sample(randomCriteria,8)
+    password = random.sample(randomCriteria,16)
     password += random.sample(string.ascii_uppercase,2)
     password += random.choice(string.digits)
     password += random.choice(string.punctuation)
